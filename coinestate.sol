1c6bd5c427d1dbb4447fede7f8a0219501d5727d
SOMIDAX <23489369+somidax@users.noreply.github.com>
2018-03-22 10:55:33 +0100

    update to required function for better deployment
    change from assert to require in safeMath

--- coinestate.sol
+++ coinestate.sol
@@ -1,20 +1,20 @@
-pragma solidity ^0.4.20;
+pragma solidity 0.4.21;
 
 contract SafeMath {
   function safeMul(uint a, uint b) pure internal returns (uint) {
     uint c = a * b;
-    assert(a == 0 || c / a == b);
+    require(a == 0 || c / a == b);
     return c;
   }
 
   function safeSub(uint a, uint b) pure internal returns (uint) {
-    assert(b <= a);
+    require(b <= a);
     return a - b;
   }
 
   function safeAdd(uint a, uint b) pure internal returns (uint) {
     uint c = a + b;
-    assert(c>=a && c>=b);
+    require(c>=a && c>=b);
     return c;
   }
 }
